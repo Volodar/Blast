218f21377fc060bdfdd98f4100001c8efc653c0c
Vladimir Tolmachev <tolm_vl@hotmail.com>
2025-10-15 16:08:19 +0700

    push release version v1

---
+++ docs/src/settings.34f5a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/blast.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",main:"47f0c"}};
